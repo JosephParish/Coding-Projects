1db2471bb76d78a5826df790084f958469f19acf
Joseph Parish <113930162+JosephParish@users.noreply.github.com>
2025-09-11 22:53:31 +0100

    Add files via upload

---
+++ Uni Year 2/3. Web Server and Client/Server/InsertionSort.java
@@ -0,0 +1,27 @@
+/**
+ * <p> InsertionSort Algorithm to sort unsorted arrays,
+ * does this by placing elements in the correct place one by one.</p>
+ *
+ * @author Joseph Parish.
+ * @version 1.0.0
+ * Last Changed: 01/05/25
+ */
+public class InsertionSort {
+    /**
+     * Main body, loops though elements
+     * placing current element behind all larger elements behind it
+     *
+     * @param array - array to be sorted
+     */
+    void InsertionSort(int array[])
+    {
+        for (int i = 1; i < array.length; ++i) {
+            int j = i - 1;
+            while (j >= 0 && array[j] > array[i]) {
+                array[j + 1] = array[j];
+                j = j - 1;
+            }
+            array[j + 1] = array[i];
+        }
+    }
+}
