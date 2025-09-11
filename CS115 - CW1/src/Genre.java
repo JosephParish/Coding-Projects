51bf425408915eb4dad3fc3107d8c2d591a70bb0
Joseph Parish <113930162+JosephParish@users.noreply.github.com>
2025-09-11 20:18:05 +0100

    uploading Library system

---
+++ CS115 - CW1/src/Genre.java
@@ -0,0 +1,10 @@
+/**
+ * <p>A enum class for all Genres</p>
+ *
+ * @author Joseph Parish
+ * @version 1.0.0
+ */
+public enum Genre
+{
+    FANTASY, MYSTERY, SCIENCE_FICTION, ROMANCE, NON_FICTION
+}
